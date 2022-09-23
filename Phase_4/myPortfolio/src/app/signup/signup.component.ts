1fda7eb49e716e16f1668f13fdf889294eb3ed9f
Deeptisingh29-tech <deepti.singh_cs18@gla.ac.in>
2022-09-23 20:42:43 +0530

    Phase 4 lesson 3 project

---
+++ Phase_4/myPortfolio/src/app/signup/signup.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Login } from '../login';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  loginRef = new FormGroup({
+    email:new FormControl(),
+    pass:new FormControl()
+  });
+  msg:string = '';
+
+  loginDetails:Array<Login>=[]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  createAccount(){
+    let login = this.loginRef.value;
+    let l = {email:login.email, pass:login.pass};
+    let result = this.loginDetails.find(obj => obj.email == l.email);
+    if(result == undefined){
+      this.loginDetails.push(l);
+      sessionStorage.setItem("loginInfo", JSON.stringify(this.loginDetails));
+      this.msg = "Account created successfully ....";
+    }else{
+      this.msg = "Email must be unique....";
+    }
+
+    this.loginRef.reset();
+    
+  }
+
+}
